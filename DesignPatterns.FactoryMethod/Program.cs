b918ad59845a20179a4f007097a7194d5d7d0817
tuyur <mac@tuyur.local>
2020-03-19 21:13:26 +0300

    factory method design pattern c#

---
+++ DesignPatterns.FactoryMethod/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DesignPatterns.FactoryMethod
+{
+    public interface ILoggerFactory{
+        ILogger CreateLogger();
+    }
+
+    public class LoggerFactoryY : ILoggerFactory{
+        public ILogger CreateLogger(){
+            return new YLogger();
+        }
+    }
+
+    public interface ILogger{
+        void log();
+    }
+
+    public class TLogger : ILogger{
+        public void log(){
+            System.Console.WriteLine("logged by Tlogger");
+        }
+    }
+
+    public class YLogger :ILogger{
+        public void log(){
+            System.Console.WriteLine("logged by Ylogger");
+        }
+    }
+
+    public class ProductManager{
+        private readonly ILoggerFactory _loggerFactory;
+
+        public ProductManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save(){
+            System.Console.WriteLine("saved");
+
+            // ILoggerFactory loggerFactory = new LoggerFactoryY();
+
+            var logger = _loggerFactory.CreateLogger();
+            logger.log();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new LoggerFactoryY());
+            productManager.Save();
+        }
+    }
+}
