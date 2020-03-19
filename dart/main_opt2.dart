40a8e2cf090188cf3875d2113aca7341728a8d34
tuyur <tuyur4444@gmail.com>
2020-03-19 21:27:12 +0300

    factory pattern dart

---
+++ dart/main_opt2.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+void main() {
+  final circle = Shape('circle');
+  final square = Shape('square');
+
+  print(circle.area);
+  print(square.area);
+}
+
+abstract class Shape {
+  num get area;
+
+  factory Shape(String type) {
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Can\'t create $type';
+  }
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
